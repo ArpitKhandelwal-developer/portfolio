252418622ac23e5c49fe5f46162c83322bec7592
Arpit Khandelwal <71580150+ArpitKhandelwal-developer@users.noreply.github.com>
2023-05-02 21:51:34 +0530

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -5,11 +5,11 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    title: "Machine Learning Enginner",
+    cardImage: "assets/images/experience-page/Coincent_PNG.png",
+    place: "Coincent",
+    time: "(Dec, 2022 - Feb, 2023)",
+    desp: "<li>Worked as Machine learning Intern.</li> <li>Learned Machine Learning. </li> <li></li>",
   },
   {
     title: "Student Developer",
