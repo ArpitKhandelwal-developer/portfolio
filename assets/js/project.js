3951fb9eab6965aaf51c5efbb226ffed88b2cd5b
Arpit Khandelwal <71580150+ArpitKhandelwal-developer@users.noreply.github.com>
2023-05-03 12:51:12 +0530

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -6,12 +6,12 @@ const projectcards = document.querySelector(".projectcards");
 // Array of object for projects
 const projects = [
   {
-    title: "Quiz App",
-    cardImage: "assets/images/project-page/quiz.jpg",
-    description: "A quiz app built using basic react.",
+    title: "Music Player In Python",
+    cardImage: "assets/images/project-page/music.png",
+    description: "A music player built in Python.",
     tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
     Previewlink: "",
-    Githublink: "",
+    Githublink: "https://github.com/ArpitKhandelwal-developer/CodeClause_MusicPlayer",
   },
   {
     title: "Chess Engine",
